21ee238530cc7109d158257ebb3949427b1caf7f
Your Name <you@example.com>
2021-07-02 16:01:05 +0900

    co

---
+++ todo/src/main/java/jp/kobespiral/narimatsu/todo/entity/Member.java
@@ -0,0 +1,20 @@
+package jp.kobespiral.narimatsu.todo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member {
+    @Id
+    String mid;   //メンバーID
+    String name;  //氏名
+    String password; //パスワード（暗号化済）
+    String role; //ロール
+}
